e2298fdc08de173972190b9484e53118348fbc94
Giovanni. K <giovannibrandonkornet@gmail.com>
2025-05-08 15:05:53 +0200

    Added transitions

--- front-page.php
+++ front-page.php
@@ -11,37 +11,40 @@ get_header();
                     <h1 class="headingText mb-6 max-w-8/12"><?php the_field('title'); ?></h1>
                     <p class="bodyText mb-6"><?php echo esc_html(get_field('body')); ?></p>
                     <div class="flex flex-row">
-                        <a href="" class="CTAButton flex items-center">
+                        <a href="#" class="CTAButton flex items-center transition-colors duration-200">
                             <?php echo esc_html(get_field('button')); ?>
-                            <img class="ml-2.5" src="<?php the_field('cta_button_icon'); ?>" alt="">
+                            <img class="ml-2.5" src="<?php echo esc_url(get_field('cta_button_icon')); ?>" alt="">
                         </a>
                     </div>
                 </div>
             </div>
             <div class="grid grid-rows-1 gap-5 grid-cols-1 md:grid-cols-2 mt-20 mb-24">
                 <!-- Hele block clickable. Inline CSS voor variable background img. -->
-                <a href="" class="CTAContainer flex flex-col justify-end rounded-sm" style="background-image: linear-gradient(180deg,rgba(20, 20, 20, 0) 0%, rgba(20, 20, 20, 0.8) 100%), url(<?php the_field('cta_background_1'); ?>);">
+                <a href="#" class="CTAContainer flex flex-col justify-end rounded-sm"
+                    style="background-image: linear-gradient(180deg,rgba(20, 20, 20, 0) 0%, rgba(20, 20, 20, 0.8) 100%), url(<?php the_field('cta_background_1'); ?>);">
                     <div class="CTAContent">
                         <div class="flex flex-row justify-between">
                             <div class="content-end">
                                 <h2 class="CTATitle mb-2"><?php echo esc_html(get_field('cta_title_1')); ?></h2>
                                 <p class="CTABody"><?php echo esc_html(get_field('cta_body_1')); ?></p>
                             </div>
-                            <div class="border-2 border-solid border-white/30 hover:border-white/50 rounded-sm p-4">
+                            <div class="border-2 border-solid border-white/30 hover:border-white/50 transition-colors duration-200 rounded-sm p-4">
                                 <img src="<?php the_field('cta_blocks_icon'); ?>" alt="">
                             </div>
                         </div>
                     </div>
                 </a>
                 <!-- Alleen icon clickable. Inline CSS voor variable background img. -->
-                <div class="CTAContainer flex flex-col justify-end rounded-sm" style="background-image: linear-gradient(180deg,rgba(20, 20, 20, 0) 0%, rgba(20, 20, 20, 0.8) 100%), url(<?php the_field('cta_background_2'); ?>);">
+                <div class="CTAContainer flex flex-col justify-end rounded-sm"
+                    style="background-image: linear-gradient(180deg,rgba(20, 20, 20, 0) 0%, rgba(20, 20, 20, 0.8) 100%), url(<?php the_field('cta_background_2'); ?>);">
                     <div class="CTAContent">
                         <div class="flex flex-row justify-between">
                             <div class="content-end">
                                 <h2 class="CTATitle mb-2"><?php echo esc_html(get_field('cta_title_2')); ?></h2>
                                 <p class="CTABody"><?php echo esc_html(get_field('cta_body_2')); ?></p>
                             </div>
-                            <a href="" class="border-2 border-solid border-white/30 hover:border-white/50 rounded-sm p-4">
+                            <a href="#"
+                                class="border-2 border-solid border-white/30 hover:border-white/50 transition-colors duration-200 rounded-sm p-4">
                                 <img src="<?php the_field('cta_blocks_icon'); ?>" alt="">
                             </a>
                         </div>
